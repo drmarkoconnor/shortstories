4196d542fb1d313c2086312031afae15fea431c4
Dr Mark O'Connor <dr.mark.oconnor@googlemail.com>
2021-03-31 11:35:33 +0100

    took out year variable in footer and commented out code line in .eleventy.js as causing build failure

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,26 @@
 const storyList = document.querySelectorAll('.story-list');
+const cardTitle = document.querySelector('.card-title');
+const card = document.querySelector('.card');
+const cardBody = document.querySelector('.card-body');
+const cardText = document.querySelector('.card-text');
+const stories =
+  // loops through all story-list class elements
+  // if it has been clicked ( the e.target )
+  // then toggles the overText class ( which pushes it right presently)
+  // then loops through all lis again and for those
+  // li elements that have the sticky out overText class, removes it.
+  // the css adds a smooth transition to it
+  storyList.forEach((story) => {
+    story.addEventListener('click', (e) => {
+      e.target.classList.toggle('overText');
 
-// loops through all li elements
-// if it has been clicked ( the e.target )
-// then toggles the overText class ( which pushes it right presently)
-// then loops through all lis again and for those
-// li elements that have the sticky out overText class, removes it.
-// the css adds a smooth transition to it
-storyList.forEach((story) => {
-  story.addEventListener('click', (e) => {
-    e.target.classList.toggle('overText');
-    console.log(e.target);
-    storyList.forEach((story) => {
-      if (story != e.target) {
-        story.classList.remove('overText');
-      }
+      cardTitle.innerHTML = e.target.innerHTML;
+      cardText.innerHTML = e.target.innerHTML;
+
+      storyList.forEach((story) => {
+        if (story != e.target) {
+          story.classList.remove('overText');
+        }
+      });
     });
   });
-});
