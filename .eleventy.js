e9ab7dcf7bc21406147963b87626cf6bc2adb056
Dr Mark O'Connor <dr.mark.oconnor@googlemail.com>
2021-03-31 14:30:25 +0100

    reintroduced year variable as build now set with eleventy

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,4 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy('assets');
-  // eleventyConfig.addShortcode('year', () => `${new Date().getFullYear()}`);
+  eleventyConfig.addShortcode('year', () => `${new Date().getFullYear()}`);
 };
