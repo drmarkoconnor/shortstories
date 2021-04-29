c3e3d2b5f5884b02db33432137df050f33b51177
Dr Mark O'Connor <dr.mark.oconnor@googlemail.com>
2021-04-29 20:34:20 +0100

    commit from vscode

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,7 @@
+const eleventyNavigation = require('@11ty/eleventy-navigation');
+
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy('assets');
   eleventyConfig.addShortcode('year', () => `${new Date().getFullYear()}`);
+  eleventyConfig.addPlugin(eleventyNavigation);
 };
--- _data/storyData.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  story1: ['angelo', 'moc', 'A racy number with excellent extended metaphors'],
-};
